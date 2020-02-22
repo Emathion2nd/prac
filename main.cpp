6c57f6aece63ab6c975a1d6d4c0b46c9be22b5b9
Amanuel Chukala <amanuel.chukala@ucdenver.edu>
2020-02-22 02:07:27 -0700

    my second commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
 
 int main() {
 
 	std::cout << "Hello, World!" << std::endl; //std::endl;
+
+
+
+	#define const double PI = 3.14;
+	srand(time(0));
+	int numToBeGuessed = 10;//random(10);
+	string input;
+	int num = rand() % 10;
+
+	do{
+		cout << num;
+		cout << "Guess a number ";
+		getline(cin,input);
+		numToBeGuessed = stoi(input);
+	}while (num != numToBeGuessed);
+
+
+
 
 	return 0;
 }
